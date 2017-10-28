e6cef82ba01d6420745e2eacf2e2ef10fa8cb9ac
Dylan Cunningham <gdylanc@gmail.com>
2017-10-28 16:25:50 -0700

    a few more routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex.js');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next){
+  knex.raw(`select * from animals`)
+  .then(function(animals){
+    res.render('index', {animals: animals.rows})
+  })
+})
+
+
+
+router.get('/:id', function(req, res, next) {
+  knex.raw(`select * from animals where id=${req.params.id}`)
+  .then(function(animal){
+    res.render('id', { animal: animal.rows[0] });
+    // res.json(animal);
+
+  })
 });
+
+router.get('/add/:name/:species/:gender', function(req, res, next){
+  knex.raw(`insert into animals (name, species, gender) values ('${req.params.name}', '${req.params.species}', '${req.params.gender}')`)
+  .then(function(animals){
+    res.redirect('/1');
+  })
+})
 
 module.exports = router;
